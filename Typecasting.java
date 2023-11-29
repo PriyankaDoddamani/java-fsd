8695fe335937c061a56742615e3012a3a98ca113
PriyankaDoddamani <151015678+PriyankaDoddamani@users.noreply.github.com>
2023-11-29 12:49:45 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,18 @@
+package project01;
+
+public class Typecasting {
+
+	public static void main(String[] args) {  
+	//impilict type
+		int a=1;
+		float b=a;
+		System.out.println("output of b" + b);
+		
+		//expilict type
+		float c=2.20f;
+		int d=(int)c;
+		System.out.println("output of d" + d);
+
+	}
+
+}
