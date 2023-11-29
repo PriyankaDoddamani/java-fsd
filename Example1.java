b8274b6bf7cce927cdf414798a7a3221b94d7971
PriyankaDoddamani <151015678+PriyankaDoddamani@users.noreply.github.com>
2023-11-29 12:38:15 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,30 @@
+package project6;
+
+
+		// TODO Auto-generated method stub
+
+		class MyException extends Exception{
+			   String str1;
+			   MyException(String str2) {
+				str1=str2;
+			   }
+			   public String toString(){ 
+				return ("MyException Occurred: "+str1) ;
+			   }
+			   class Example1{
+
+			}
+			   public static void main(String args[]){
+				try{
+					System.out.println("Starting of try block");
+					// I'm throwing the custom exception using throw
+					throw new MyException("This is My error Message");
+				}
+				catch(MyException exp){
+					System.out.println("Catch Block") ;
+					System.out.println(exp) ;
+				}
+				
+	}
+
+}
