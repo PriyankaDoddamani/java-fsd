1184d7681448214eca0ba73538fc368ffcd2f204
PriyankaDoddamani <151015678+PriyankaDoddamani@users.noreply.github.com>
2023-11-29 12:07:05 +0530

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,25 @@
+package project1;
+
+public class MyThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		{
+		 	{
+		  		System.out.println("concurrent thread started running..");
+		}
+		 	{
+		  		MyThread mt = new  MyThread();
+		  		mt.start();
+		 	}
+		}
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
