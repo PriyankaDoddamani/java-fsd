68c028ab70a049c15e7019c2c0e443355aecb002
PriyankaDoddamani <151015678+PriyankaDoddamani@users.noreply.github.com>
2023-12-04 17:08:17 +0530

    Add files via upload

---
+++ InnerClassAssisted3.java
@@ -0,0 +1,20 @@
+package project07;
+
+public class InnerClassAssisted3 {
+
+		// TODO Auto-generated method stub
+
+
+			public static void main(String[] args) {
+			AnonymousInnerClass i = new AnonymousInnerClass() {
+
+			         public void display() {
+			            System.out.println("Anonymous Inner Class");
+			         }
+			      };
+			      i.display();
+			   }
+			
+	}
+
+
